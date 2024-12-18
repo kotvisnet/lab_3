86c7ca1914b49f09c89d2c2eef760727236339a6
Polina <poliinaaross@gmail.com>
2024-12-18 19:26:38 +0300

    re expression

--- RegularExpression.py
+++ RegularExpression.py
@@ -2,13 +2,14 @@ import re
 
 pattern = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$"
 
-def is_valid_card(card: str):
-    if not isinstance(card, str):
-        raise TypeError("card must be str!")
-    return re.match(pattern, card) is not None
+def is_valid_password(password: str):
+    if not isinstance(password, str):
+        raise TypeError("password must be str!")
+    return re.match(pattern, password) is not None
 
-def find_card_in_text(text: str):
+def find_password_in_text(text: str):
     if not isinstance(text, str):
         raise TypeError("The provided text must be a string.")
-    cards = re.findall(pattern, text)
-    return cards
+    passwords = re.findall(pattern, text)
+    return passwords
+
