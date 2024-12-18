3713bf201c39a16cf0afa86580bd91897df71c82
Polina <poliinaaross@gmail.com>
2024-12-18 21:59:45 +0300

    search

---
+++ main.py
@@ -0,0 +1,6 @@
+from PageSearch import find_passwords_on_webpage
+from fileSearch import find_passwords_in_file
+
+find_passwords_on_webpage('https://github.com/kotvisnet/lab_3/tree/master')
+
+print(find_passwords_in_file('passwords.txt'))
